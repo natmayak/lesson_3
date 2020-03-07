6cba1910a6a1180b79ff62cedc3123fa3b888d63
Nat Nikulina <natalie.nikulina@gmail.com>
2020-03-07 23:06:05 +0300

    week 3 hw, 07.03 commit

--- datetime_hw.py
+++ datetime_hw.py
@@ -2,18 +2,18 @@ from datetime import datetime, timedelta
 today = datetime.now()
 delta = timedelta(days=1)
 yesterday = today - delta
-yesterday
-datetime.datetime(2020, 2, 28, 11, 44, 0, 57526)
+print(yesterday)
+#datetime.datetime(2020, 2, 28, 11, 44, 0, 57526)
 delta_month = timedelta(days=30)
 month_ago = today - delta_month
-month_ago
-datetime.datetime(2020, 1, 30, 11, 44, 0, 57526)
-today.strftime('%A %d %B %Y')
-'Saturday 29 February 2020'
-text_date = '12/15/2019'
-text_date_converted = datetime.strptime(text_date, '%m/%d/%Y')
-text_date_converted
-datetime.datetime(2019, 12, 15, 0, 0)
+print(month_ago)
+#datetime.datetime(2020, 1, 30, 11, 44, 0, 57526)
+print(today.strftime('%A %d %B %Y'))
+#'Saturday 07 March 2020'
+text_date = '12/15/2019 12:10:03.234567'
+text_date_converted = datetime.strptime(text_date, '%m/%d/%Y %H:%M:%S.%f')
+print(text_date_converted)
+#datetime.datetime(2019, 12, 15, 0, 0)
 
 
 
